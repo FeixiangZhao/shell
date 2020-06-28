ab3bc7ae78eb138aa8bbaf8acc5c23924cb5a424
FeixiangZhao <65398966+FeixiangZhao@users.noreply.github.com>
2020-06-28 16:17:58 +0800

    Update test_git.sh
    Add the 'new' error result.

--- test_git.sh
+++ test_git.sh
@@ -25,3 +25,6 @@ D:\my_code_soft\my_github\shell (master -> origin)
 error: pathspec 'new' did not match any file(s) known to git
 error: pathspec 'file'' did not match any file(s) known to git
 
+## 以上报错的原因，在于 commit -m 后的提交备注，没有使用双引号，而使用了单引号，单引号，git命令中就认为是文件或其他信息，进而判断有失败
+## 不用的语言，语法中单引号或双引号的用法不同，有时会区分，有时不会区分
+## 在 git 中备注信息，一定要用双引号，不能用单引号
